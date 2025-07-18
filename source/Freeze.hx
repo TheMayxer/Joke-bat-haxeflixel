50df53d562467caa21cca9e03f993d1753e32e5a
TheMayxer <themaxxer74@gmail.com>
2025-07-18 12:15:41 -0300

    changed somethings

--- source/Freeze.hx
+++ source/Freeze.hx
@@ -8,7 +8,6 @@ class Crached extends FlxState
 {
     public function new() {
         super();
-        FlxG.cameras.bgColor = 0x1E78FF;
         var shit = new FlxSprite().loadGraphic("images/message.png");
         shit.screenCenter();
         add(shit);
